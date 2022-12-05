64ea0329ffea849c5cfd812748e07b669f428cab
Victor-2691 <victor.salgado.martinez@cuc.cr>
2022-12-05 08:39:54 -0600

    Modulo_Periodo_Completo

--- ProyectoII_PrograV_ConsumeAPI/JavaScript/GestionOperaciones.js
+++ ProyectoII_PrograV_ConsumeAPI/JavaScript/GestionOperaciones.js
@@ -1,20 +1,5 @@
-﻿* Esta funcion se encarga de mostrar la pantalla se confirmacion al eliminar un dato * /
-function Conformar() {
+﻿
 
-    /*El objeto confirm de javascrip es quien se encarga de mostrar la venta*/
-
-    if (confirm("Esta seguro que desea eliminar este registro ?") == true) {// true indica yes
-
-        /*.getElementById se encarga de obtener los id html en aspx 
-         valor (es el id del HiddenField de c#) */
-
-        /*Se le asigna el valor con value*/
-        document.getElementById('valor').value = 'si';
-    }
-    else {// false incia not
-        document.getElementById('valor').value = 'no';
-    }
-}
 
 function Alertar(mensaje) {
     alert(mensaje)
@@ -24,6 +9,27 @@ function CambiarDiv() {
     box = document.getElementById('divTabla')[0];
     box.style.width = '100px';
     box.style.height = '100px';
+}
+
+
+function Confirmar() {
+
+
+    if (confirm("Esta seguro que desea eliminar este registro ?") == true) {// true indica yes
+
+        /*.getElementById se encarga de obtener los id html en aspx 
+         valor (es el id del HiddenField de c#) */
+
+        /*Se le asigna el valor con value*/
+        document.getElementById('Codi').value = 'si';
+    }
+    else {// false incia not
+    }
+}
+
+function regresar() {
+
+    window.location("~/Paginas/Cursos.aspx")
 }
 
 
